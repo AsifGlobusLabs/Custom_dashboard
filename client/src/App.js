2b83148d5fc64edf521254df37c47b57cb4c9626
AsifGlobusLabs <asif.globuslabs@gmail.com>
2024-11-14 12:05:34 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import DashboardLayoutBasic from './component/DashboardLayoutBasic';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DashboardLayoutBasic />
+    </div>
+  );
+}
+
+export default App;
